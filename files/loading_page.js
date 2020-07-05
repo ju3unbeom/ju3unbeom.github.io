ef009c4baf7fbc754d84723f4b5cb0a9a1f57a1e
eunbeom.kang <eunbeom.kang@linecorp.com>
2020-07-06 01:37:32 +0900

    일단 배포

---
+++ files/loading_page.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+	requestAnimationFrame(function() {
+			document.getElementById("loadingBar").style.width = "100%";
+	  });
+});
+
+window.onload = function () {
+document.getElementById("loadingIndicator").style.opacity = "0";
+setTimeout(function() {
+    document.getElementById("loadingIndicator").style.display = "none";}, 450);
+}
